204b5991c791d8d4cf8bea450cd279ea63ed4c94
ElijahHHall <ehall851@gmail.com>
2018-03-21 19:04:52 -0600

    commit 4 - FizzBuzz answered.

--- answers.js
+++ answers.js
@@ -7,3 +7,36 @@ for (i=0; i <= 200; i+=2) {
 	console.log(i);
 }
 
+for (i=0; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
+}
+
+for (i=0; i <= 20; i+=2) {
+		Math.random();
+		console.log("...the catnip made me do it...");
+}
+// 
+
+
+
+for (i = 1; i <= 100; i++) {
+	console.log(i);
+}
+
+for (i = 1; i <= 100; i++) {
+	if (i % 3 === 1);
+		console.log("Fizz");
+} 
+
+for (i = 1; i <= 100; i++) {
+	if (i % 5 === 1);
+		console.log("Buzz");
+} 
+
+for (i = 1; i <= 100; i++) {
+	if (i % 3 === 1 && i % 5 === 1);
+		console.log("FizzBuzz");
+} 
+
+
+
